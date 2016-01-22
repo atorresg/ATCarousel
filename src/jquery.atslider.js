bd3a1b89ef34e5a3addec3da2f82ebc5e07c0293
Torres, Alejandro <Alejandro.torres@wunderman.com>
2016-01-22 19:35:51 -0300

    taking work to home

--- src/jquery.atslider.js
+++ src/jquery.atslider.js
@@ -1,18 +1,45 @@
 (function($){
 	$.fn.ATSlider = function(){
-		var config, slider;
+		var config, slider, length, args, container;
 		slider=$(this);
-		console.log(arguments);
+		slider.addClass("ATSlider");
+		length=slider.find('img').length;
+		args = arguments;
 		init();
 
 		function test(){
-			console.log(slider.find('img').length);
+			console.log(config);
 		}
+
 		function init(){
-			slider.addClass("ATSlider");
+			config={
+				width:slider.find('ul').outerWidth(true),
+				totalWidth: 0,
+				circle:true,
+				buttons:false,
+				speed:500,
+				easing:"swing",
+				autoPlay:true,
+				hoverStop:true
+			};
+
+			if ( args.length>0 && typeof args[0]==="object"){
+				for (var i in args[0]){
+					config[i]=args[0][i];
+				}
+			}
+			slider.append('<div class="ATContainer"></div>');
+			container = slider.find('.ATContainer');
+			var list= slider.find('ul').html();
+			container.append('<ul>'+list+(config.circle?list:'')+'</ul>');
+			slider.find('ul').remove();
+			if (config.circle===true){
+
+			}
 		}
+
 		return {
 			test:test
-		}
-	}
+		};
+	};
 }(jQuery));
